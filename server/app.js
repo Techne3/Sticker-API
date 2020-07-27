fa9977dbaf0182658c6229b3accfc44b2d880fa6
Timothy Sharkey <Techne3@gmail.com>
2020-07-27 15:55:43 -0600

    Test are up and running

--- server/app.js
+++ server/app.js
@@ -1,34 +1,35 @@
-require("dotenv").config();
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const stickers = require("./api/stickers.js");
-// view engine setup
+const stickers = require("./api/stickers");
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/v1/stickers", stickers);
 
-const port = process.env.PORT || 5005;
-app.listen(port, () => {
-  console.log(`server is up on ${port}!`);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // render the error page
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get("env") === "development",
+    error: req.app.get("env") === "development" ? err : {},
   });
 });
 
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -1,10 +1,31 @@
 const knex = require("../db/knex");
+const expect = require("chai").expect;
+const request = require("supertest");
+const app = require("../app");
+const fixtures = require("./fixtures");
+
 describe("CRUD Stickers", () => {
-  before(() => {
+  before((done) => {
     // run migrations
-    knex.migrate.latest().then(() => {
-      // run seeds
-      return knex.seed.run();
-    });
+    knex.migrate
+      .latest()
+      .then(() => {
+        // run seeds
+        return knex.seed.run();
+      })
+      .then(() => done());
+  });
+  it("List all records", (done) => {
+    request(app)
+      .get("/api/v1/stickers")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a("array");
+        // console.log(response.body);
+        expect(response.body).to.deep.equal(fixtures.stickers);
+        done();
+      });
   });
 });
